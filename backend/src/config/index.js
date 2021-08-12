e4ecae68ade7d199412bff7df7d5efa4a7ab0d2b
Gustavo Slomski <gustavoslomski@alunos.utfpr.edu.br>
2021-08-12 12:50:22 -0300

    primeira versao do editor

---
+++ backend/src/config/index.js
@@ -0,0 +1,9 @@
+'use-strict'
+
+module.exports = {
+  dslFileExt: 'oab', // Add and change this
+  paths: {
+    dslCompiler: './compiler/dsl_compiler.jar', // Add this
+    languageServer: './language_server/language_server.jar'
+  }
+}
